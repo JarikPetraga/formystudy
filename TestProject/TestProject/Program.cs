5a938a88d7a6c11a88e109da2e5431a6fb38ef51
JarikPetraga <petraga@mail.ua>
2016-12-09 00:15:22 +0200

    Update program

--- TestProject/TestProject/Program.cs
+++ TestProject/TestProject/Program.cs
@@ -11,6 +11,7 @@ namespace TestProject
         static void Main(string[] args)
         {
             Console.WriteLine("helloWorld");
+            Console.WriteLine("It was update with the branch");
             Console.WriteLine("made by Yaromyr and Dima");
 
             Console.ReadLine();
